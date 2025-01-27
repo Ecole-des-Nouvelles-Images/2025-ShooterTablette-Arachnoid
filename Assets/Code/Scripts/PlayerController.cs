12b4feb583073d8d894c9f293849cb99cc9dcdc3
Elias Prevost <e.prevost@ensi.lan>
2025-01-27 15:47:54 +0100

    CharacterPlaceHolder_Imported

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace Code.Scripts
 {
@@ -13,15 +14,32 @@ namespace Code.Scripts
         private Rigidbody _rb;
         public bool IsFiring = false;
 
-        void Start()
+        private InputAction _moveAction;
+        private InputAction _lookAction;
+
+        public Button FireButton;
+
+        private void Start()
         {
             _rb = GetComponent<Rigidbody>();
             _playerInput = GetComponent<PlayerInput>();
+
+            _moveAction = _playerInput.actions["Move"];
+            _lookAction = _playerInput.actions["Look"];
+
+            var fireButtonEventTrigger = FireButton.GetComponent<EventTrigger>();
+            if (fireButtonEventTrigger == null)
+            {
+                fireButtonEventTrigger = FireButton.gameObject.AddComponent<EventTrigger>();
+            }
+
+            AddEventTrigger(fireButtonEventTrigger, EventTriggerType.PointerDown, (eventData) => { IsFiring = true; });
+            AddEventTrigger(fireButtonEventTrigger, EventTriggerType.PointerUp, (eventData) => { IsFiring = false; });
         }
 
-        void FixedUpdate()
+        private void FixedUpdate()
         {
-            Vector2 moveInput = _playerInput.actions["Move"].ReadValue<Vector2>();
+            Vector2 moveInput = _moveAction.ReadValue<Vector2>();
             Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
 
             if (moveDirection != Vector3.zero)
@@ -29,21 +47,21 @@ namespace Code.Scripts
                 _rb.MovePosition(_rb.position + moveDirection * (_moveSpeed * Time.fixedDeltaTime));
             }
 
-            Vector2 lookInput = _playerInput.actions["Look"].ReadValue<Vector2>();
-
+            Vector2 lookInput = _lookAction.ReadValue<Vector2>();
             if (lookInput != Vector2.zero)
             {
                 Vector3 lookDirection = new Vector3(lookInput.x, 0, lookInput.y);
                 transform.rotation = Quaternion.LookRotation(lookDirection);
-                IsFiring = true;
-            }
-
-            else
-            {
-                IsFiring = false;
             }
 
             _moveSpeed = IsFiring ? _fireSpeed : _sprintSpeed;
+        }
+
+        private void AddEventTrigger(EventTrigger trigger, EventTriggerType eventType, System.Action<BaseEventData> callback)
+        {
+            var entry = new EventTrigger.Entry { eventID = eventType };
+            entry.callback.AddListener((eventData) => callback(eventData));
+            trigger.triggers.Add(entry);
         }
     }
 }
